a2446445cfa551fb4b54d926c3067571f3c64dab
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-03 09:16:45 -0500

    setting up Stack visualizer basics

--- src/components/Stack/StackVisualizer.js
+++ src/components/Stack/StackVisualizer.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-const StackVisualizer = () => {
+const StackVisualizer = ({stack}) => {
 
+  const [displayStack, setDisplayStack] = useState(stack)
+
+  useEffect(() => {
+    setDisplayStack(stack)
+    if (displayStack.items) {
+      console.log(displayStack.items.length)
+    }
+  }, [stack])
 
   return (
     <div className="stack-visualizer">
       <p>stack visualizer</p>
+      <ul>
+        {displayStack.items.length>=1
+          ? <p>displayStack</p>
+          : <p>test</p>
+        }
+      </ul>
     </div>
   )
 }
